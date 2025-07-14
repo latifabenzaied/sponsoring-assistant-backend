f90a104e617751296dd9e38abd758dc19fb7cf60
latifa <latifa.benzaied@esprit.tn>
2025-07-14 10:43:07 +0100

    initial project setup

---
+++ src/main/java/com/cercinaai/sponsoringassistant/backend/SponsoringbackendApplication.java
@@ -0,0 +1,13 @@
+package com.cercinaai.sponsoringassistant.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SponsoringbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SponsoringbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cercinaai/sponsoringassistant/backend/SponsoringbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cercinaai.sponsoringassistant.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SponsoringbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
